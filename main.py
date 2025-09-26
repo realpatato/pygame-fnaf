666765a45dff351f64d30e1b50ab0275ea514ef4
realpatato <jason.t.richardii@gmail.com>
2025-09-26 09:22:25 -0400

    added cam button, can be selected or non selected or selected, dont have names on them yet

---
+++ main.py
@@ -0,0 +1,49 @@
+''' IMPORTS '''
+import pygame
+import cam_buttons
+
+button = cam_buttons.camButton(0, 0, "2B", True)
+
+''' PYGAME SETUP '''
+#initialize the pygame module
+pygame.init()
+
+#define width and height for the screen
+screen_width = 1280
+screen_height = 720
+#create the screen
+screen = pygame.display.set_mode((screen_width, screen_height))
+#set caption for the window
+pygame.display.set_caption("FNAF")
+
+''' PRE LOOP STUFF '''
+#clock for controlling frame rate
+clock = pygame.time.Clock()
+#variable to control the game loop
+keep_playing = True
+
+''' LOOP '''
+while keep_playing:
+    #iterate over all pygame events
+    for event in pygame.event.get():
+        #check for a quit
+        if event.type == pygame.QUIT:
+            #end the loop
+            keep_playing = False
+    
+    #intial screen wipe
+    screen.fill((0, 0, 0))
+
+    button.draw_self(screen)
+
+    #update the display
+    pygame.display.update()
+    
+    #tick the clock
+    clock.tick(60)
+
+''' ENDING IT ALL :D '''
+#quit the window
+pygame.quit()
+#end the program
+quit()
